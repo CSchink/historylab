ceb9e16958bc6b74acf9cae47e1e22a968ffa709
CSchink <45950072+CSchink@users.noreply.github.com>
2020-03-12 12:51:11 -0400

    new app

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import Table from './components/table.js'
+
+
+
 function App() {
   return (
-    <div className="App">
     <Table/>
-    </div>
   );
 }
 
