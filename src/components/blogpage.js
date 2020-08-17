bedea4b24930cd8420ed2a446a6694235fefbf87
CSchink <45950072+CSchink@users.noreply.github.com>
2020-08-17 13:35:14 -0400

    stuff

--- src/components/blogpage.js
+++ src/components/blogpage.js
@@ -11,13 +11,11 @@ import { userSignUp } from "../connection";
 import MultiSelect from "./multiselect";
 
 const SignUp = () => {
-  const [fname, setFname] = useState("");
-  const [lname, setLname] = useState("");
   const [email, setEmail] = useState("");
   const [country, setCountry] = useState("");
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
-  const [passwordconfirm, setPasswordConfirm] = useState("")
+  const [passwordconfirm, setPasswordConfirm] = useState("");
 
   return (
     <MDBContainer>
@@ -28,52 +26,80 @@ const SignUp = () => {
         Welcome to SOTT Lab. Your User Profile will be your guide to the easiest
         and most effective way to use this tool.
       </p>
-      
+
       <MDBRow>
         <MDBCol md="12" className="md-0 mb-5">
           <form>
-          <div className="block-example border-top border-primary">
-            <MDBRow>
-              <MDBCol md="6">
-                <div className="md-form mb-0">
-                  <MDBInput type="text" id="contact-name" label="First name" value={fname} onChange={(event) => { setFname(event.target.value)}}/>
-                </div>
-              </MDBCol>
-              <MDBCol md="6">
-                <div className="md-form mb-0">
-                  <MDBInput type="text" id="contact-email" label="Last Name" value={lname} onChange={(event) => { setLname(event.target.value)}}/>
-                </div>
-              </MDBCol>
-            </MDBRow>
-            <MDBRow>
-              <MDBCol md="12">
-                <div className="md-form mb-0">
-                  <MDBInput type="text" id="contact-subject" label="Username" value={username} onChange={(event) => { setUsername(event.target.value)}}/>
-                </div>
-              </MDBCol>
-              <MDBCol md="12">
-                <div className="md-form mb-0">
-                  <MDBInput type="password" id="contact-subject" label="Password" value={password} onChange={(event) => { setPassword(event.target.value)}}/>
-                </div>
-              </MDBCol>
-              <MDBCol md="12">
-                <div className="md-form mb-0">
-                  <MDBInput type="password" id="contact-subject" label="Confirm Password" value={passwordconfirm} onChange={(event) => { setPasswordConfirm(event.target.value)}}/>
-                </div>
-              </MDBCol>
-            </MDBRow>
-            <MDBRow>
-              <MDBCol md="12">
-                <div className="md-form mb-0">
-                  <MDBInput type="text" id="contact-subject" label="Email" value={email} onChange={(event) => { setEmail(event.target.value)}}/>
-                </div>
-              </MDBCol>
-              <MDBCol md="12">
-                <div className="md-form mb-0">
-                  <MDBInput type="text" id="contact-subject" label="Country" value={country} onChange={(event) => { setCountry(event.target.value)}}/>
-                </div>
-              </MDBCol>
-            </MDBRow>
+            <div className="block-example border-top border-primary">
+              <MDBRow>
+                <MDBCol md="12">
+                  <div className="md-form mb-0">
+                    <MDBInput
+                      type="text"
+                      id="contact-subject"
+                      label="Username"
+                      value={username}
+                      onChange={(event) => {
+                        setUsername(event.target.value);
+                      }}
+                    />
+                  </div>
+                </MDBCol>
+                <MDBCol md="12">
+                  <div className="md-form mb-0">
+                    <MDBInput
+                      type="password"
+                      id="contact-subject"
+                      label="Password"
+                      value={password}
+                      onChange={(event) => {
+                        setPassword(event.target.value);
+                      }}
+                    />
+                  </div>
+                </MDBCol>
+                <MDBCol md="12">
+                  <div className="md-form mb-0">
+                    <MDBInput
+                      type="password"
+                      id="contact-subject"
+                      label="Confirm Password"
+                      value={passwordconfirm}
+                      onChange={(event) => {
+                        setPasswordConfirm(event.target.value);
+                      }}
+                    />
+                  </div>
+                </MDBCol>
+              </MDBRow>
+              <MDBRow>
+                <MDBCol md="12">
+                  <div className="md-form mb-0">
+                    <MDBInput
+                      type="text"
+                      id="contact-subject"
+                      label="Email"
+                      value={email}
+                      onChange={(event) => {
+                        setEmail(event.target.value);
+                      }}
+                    />
+                  </div>
+                </MDBCol>
+                <MDBCol md="12">
+                  <div className="md-form mb-0">
+                    <MDBInput
+                      type="text"
+                      id="contact-subject"
+                      label="Country"
+                      value={country}
+                      onChange={(event) => {
+                        setCountry(event.target.value);
+                      }}
+                    />
+                  </div>
+                </MDBCol>
+              </MDBRow>
             </div>
             <br></br>
             <div className="text-center block-example border-top border-primary">
@@ -92,13 +118,17 @@ const SignUp = () => {
           <br></br>
           <div className="text-center text-md-left">
             <br></br>
-            <MDBBtn color="primary" size="lg" onClick={async () => {await userSignUp({
-                user: username,
-                password: password,
-                firstname: fname,
-                lastname: lname,
-                country: country
-              })}}>
+            <MDBBtn
+              color="primary"
+              size="lg"
+              onClick={async () => {
+                await userSignUp({
+                  user: username,
+                  password: password,
+                  country: country,
+                });
+              }}
+            >
               Submit
             </MDBBtn>
           </div>
